7f362e044e047be42e1e29d632b544409bce29de
worachote1 <worachote659@gmail.com>
2025-01-08 19:25:56 +0700

    Model_conten-based: implement model with user generated by persona

--- packages/user_preference_prompt_by_persona/gernerate_preference.py
+++ packages/user_preference_prompt_by_persona/gernerate_preference.py
@@ -111,31 +111,31 @@ def create_user_persona_adventure() -> list[dict]:
 def create_user_persona_art() -> list[dict]:
 
     res_preference_prompt = [
-        {"name": "Nature & Outdoors", "score": rd.randint(0, 3), 
+        {"name": "Nature & Outdoors", "score": rd.randint(0, 1), 
         "question": "On a scale of 0-5, how much do you enjoy spending time in nature, such as mountains, beaches, national parks, waterfalls, forests, or riversides?"},
 
-        {"name": "Cultural & Historical", "score": rd.randint(0, 3), 
+        {"name": "Cultural & Historical", "score": rd.randint(0, 2), 
         "question": "On a scale of 0-5, how much do you enjoy exploring cultural and historical attractions like temples, museums, landmarks, or monuments?"},
 
-        {"name": "Urban & City Life", "score": rd.randint(0, 3), 
+        {"name": "Urban & City Life", "score": rd.randint(0, 1), 
         "question": "On a scale of 0-5, how much do you enjoy urban experiences like shopping, walking streets, cityscapes, markets, or nightlife?"},
 
-        {"name": "Relaxation & Meditation", "score": rd.randint(0, 3), 
+        {"name": "Relaxation & Meditation", "score": rd.randint(0, 1), 
         "question": "On a scale of 0-5, how much do you enjoy relaxing activities like meditation, cozy snuggles, or peaceful foggy settings?"},
 
-        {"name": "Adventure & Activities", "score": rd.randint(0, 3), 
+        {"name": "Adventure & Activities", "score": rd.randint(0, 1), 
         "question": "On a scale of 0-5, how much do you enjoy adventure activities like hiking, cycling, camping, or diving?"},
 
-        {"name": "Wildlife & Family-Friendly", "score": rd.randint(0, 3), 
+        {"name": "Wildlife & Family-Friendly", "score": rd.randint(0, 1), 
         "question": "On a scale of 0-5, how much do you enjoy visiting family-friendly places like zoos or waterparks with activities suitable for kids?"},
 
-        {"name": "Arts & Architecture", "score": rd.randint(4, 5), 
+        {"name": "Arts & Architecture", "score": 5, 
         "question": "On a scale of 0-5, how much do you enjoy exploring art and architecture during your travels?"},
 
-        {"name": "Small Villages & Rural Life", "score": rd.randint(0, 3), 
+        {"name": "Small Villages & Rural Life", "score": rd.randint(0, 1), 
         "question": "On a scale of 0-5, how much do you enjoy visiting small villages and experiencing rural life?"},
 
-        {"name": "Islands & Beaches", "score": rd.randint(0, 3), 
+        {"name": "Islands & Beaches", "score": rd.randint(0, 1), 
         "question": "On a scale of 0-5, how much do you enjoy visiting islands and spending time on beaches?"},
     ]
 
