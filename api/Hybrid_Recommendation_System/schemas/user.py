1cfa5652637acb79d2b7fff920218c4d01355d03
worachote1 <worachote659@gmail.com>
2025-01-28 08:16:54 +0700

    recSys_api: fix userinput type

--- api/Hybrid_Recommendation_System/schemas/user.py
+++ api/Hybrid_Recommendation_System/schemas/user.py
@@ -2,42 +2,7 @@ from pydantic import BaseModel
 from typing import List, Dict
 
 class AttractionTagFields(BaseModel):
-    Tourism: float
-    Adventure: float
-    Meditation: float
-    Art: float
-    Cultural: float
-    Landscape: float
-    Nature: float
-    Historical: float
-    Cityscape: float
-    Beach: float
-    Mountain: float
-    Architecture: float
-    Temple: float
-    WalkingStreet: float
-    Market: float
-    Village: float
-    NationalPark: float
-    Diving: float
-    Snuggle: float
-    Waterfall: float
-    Island: float
-    Shopping: float
-    Camping: float
-    Fog: float
-    Cycling: float
-    Monument: float
-    Zoo: float
-    Waterpark: float
-    Hiking: float
-    Museum: float
-    Riverside: float
-    NightLife: float
-    Family: float
-    Kid: float
-    Landmark: float
-    Forest: float
+    Dict[str, float]
 
 class AttractionTagScore(BaseModel):
     attractionTagFields: AttractionTagFields
